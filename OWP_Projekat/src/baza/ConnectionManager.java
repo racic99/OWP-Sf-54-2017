afb5cc42b6524bd4c890d890e85abb95bd0a1a66
Filip <filip.racic19@gmail.com>
2020-01-26 20:59:58 +0100

    Aplikacija povezana sa bazom u koju su dodati dosadasnji staticki podaci

---
+++ OWP_Projekat/src/baza/ConnectionManager.java
@@ -0,0 +1,39 @@
+package baza;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+	private static final String DATABASE_NAME = "bioskop.db";
+	
+	private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	private static final String WINDOWS_PATH = "C:" + FILE_SEPARATOR + "Baza" + FILE_SEPARATOR + DATABASE_NAME;
+	private static final String LINUX_PATH = "SQLite" + FILE_SEPARATOR + DATABASE_NAME;
+	
+	private static final String PATH = WINDOWS_PATH;
+	
+	private static Connection connection;
+	
+	public static void open() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:" + PATH);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		return connection;
+	}
+	
+	public static void close() {
+		try {
+			connection.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+}
